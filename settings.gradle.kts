718bd6e9fd796e68f352c358d5e6edb777a01cd0
gujiu21L <2897795436@qq.com>
2024-02-03 15:19:40 +0800

    我瞎捣鼓着，懵了逼啦

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndroidStudio_Git_Test01"
+include(":app")
+ 
